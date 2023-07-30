e69631190786c1bf880bfe4ed29551e749db08f8
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-07-30 23:07:57 +0000

    fin

---
+++ lesson6-1.rb
@@ -0,0 +1,9 @@
+puts "計算をはじめます"
+puts "2つの数字を入力してください"
+
+input_number_1 = gets.to_i
+input_number_2 = gets.to_i
+
+puts "計算結果を出力します"
+puts "#{input_number_1}*#{input_number_2}=#{input_number_1*input_number_2}"
+puts "計算を終了します"
